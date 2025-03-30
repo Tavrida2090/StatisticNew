7e86dabb62080ec895672df7acf754efb1fabac6
Rustam Korsovetskiy <rustamkorsovetskiy2013@gmail.com>
2025-03-30 19:43:33 +0300

    New test coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,5 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
-}
 
+    @Test
+    void findMaxInTheMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 800, 6, 11, 11, 12};
+        long expected = 800;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
